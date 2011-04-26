597b0a8f55f1c4e389ec6787c0acaf7f4f0afd17
Ryan Showalter <ryan@bixly.com>
2011-04-26 13:26:13 -0700

    Add some comments, clean up the code a bit for Example2

--- Example2/main.cpp
+++ Example2/main.cpp
@@ -58,45 +58,55 @@ void initWindow(int win_x, int win_y) {
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
 
+    // Set the clear color for when we re-draw the scene
     glClearColor(0.1, 0.1, 0.1, 1.0);
 
+    // Initialize our model's transformation matrix
     model_matrix = glm::mat4(1.0f);
     model_matrix = glm::rotate(model_matrix, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-    model_matrix = glm::translate(model_matrix, glm::vec3(0.2f, 0.0f, 0.0f));
+    model_matrix = glm::translate(model_matrix, glm::vec3(0.0f, 0.0f, 0.0f));
     model_matrix = glm::scale(model_matrix, glm::vec3(1.0f, 1.0f, 1.0f));
+
+    // Initialize our projection matrix (Gives the world a perspective feel rather than orthographic)
+    projection_matrix = glm::mat4(1.0f);
+    projection_matrix *= glm::perspective(45.0f, 4.0f/3.0f, 0.1f, 100.0f);
+
+    // Initialize our view matrix, essentially the camera or "eye" orientation in space
+    view_matrix = glm::mat4(1.0f);
+    view_matrix *= glm::lookAt(
+            glm::vec3(1.0, 3.0f, 5.0f),    // The eye's position in 3d space
+            glm::vec3(0.0f, 0.0f, 0.0f),   // What the eye is looking at
+            glm::vec3(0.0f, 1.0f, 0.0f));  // The eye's orientation vector (which way is up)
+
 }
 
 void updateWindow() {
 
-    glClearColor(0.1, 0.1, 0.1, 1.0);
+    // Clear the color and depth buffers
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
+    // Get the size of our SDL window
     int win_x, win_y;
-
     SDL_GetWindowSize(main_window, &win_x, &win_y);
 
+    // Set the viewport dimentions
     glViewport(0, 0, win_x, win_y);
 
-    projection_matrix = glm::mat4(1.0f);
-    projection_matrix *= glm::perspective(45.0f, 4.0f/3.0f, 0.1f, 100.0f);
-
-    view_matrix = glm::mat4(1.0f);
-    view_matrix *= glm::lookAt(
-            glm::vec3(1.0, 3.0f, 5.0f),
-            glm::vec3(0.0f, 0.0f, 0.0f),
-            glm::vec3(0.0f, 1.0f, 0.0f));
-
+    // Apply a slight rotation to our cube's model matrix
     model_matrix = glm::rotate(model_matrix, 1.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
 
 
 int main(int argc, char **argv) {
 
+    // Initialize our window
     initWindow(640, 480);
 
+    // Create our vertex and index vectors
     std::vector<Vertex> vert_list;
     std::vector<GLushort> index_list;
 
+    // The cube's vertices
     Vertex v0, v1, v2, v3, v4, v5, v6, v7;
     v0.x = -0.5f; v0.y =  0.5f; v0.z =  0.5f; vert_list.push_back(v0);
     v1.x =  0.5f; v1.y =  0.5f; v1.z =  0.5f; vert_list.push_back(v1);
@@ -108,9 +118,7 @@ int main(int argc, char **argv) {
     v6.x =  0.5f; v6.y = -0.5f; v6.z = -0.5f; vert_list.push_back(v6);
     v7.x = -0.5f; v7.y = -0.5f; v7.z = -0.5f; vert_list.push_back(v7);
 
-    index_list.push_back(0); index_list.push_back(1); index_list.push_back(2); // Front Face Tri 1
-    index_list.push_back(2); index_list.push_back(3); index_list.push_back(0); // Front Face Tri 2
-    
+    // The cube's faces (not represented by quads, but rather triangles)
     index_list.push_back(1); index_list.push_back(5); index_list.push_back(6); // Right Face Tri 1
     index_list.push_back(6); index_list.push_back(2); index_list.push_back(1); // Right Face Tri 2
 
@@ -120,20 +128,24 @@ int main(int argc, char **argv) {
     index_list.push_back(4); index_list.push_back(0); index_list.push_back(3); // Left Face Tri 1
     index_list.push_back(3); index_list.push_back(7); index_list.push_back(4); // Left Face Tri 2
 
-    index_list.push_back(4); index_list.push_back(5); index_list.push_back(1); // Top Face Tri 1
-    index_list.push_back(1); index_list.push_back(0); index_list.push_back(4); // Top Face Tri 2
+    index_list.push_back(0); index_list.push_back(4); index_list.push_back(5); // Top Face Tri 1
+    index_list.push_back(5); index_list.push_back(1); index_list.push_back(0); // Top Face Tri 2
 
-    index_list.push_back(3); index_list.push_back(2); index_list.push_back(6); // Bottom Face Tri 1
-    index_list.push_back(6); index_list.push_back(7); index_list.push_back(3); // Bottom Face Tri 2
+    index_list.push_back(3); index_list.push_back(0); index_list.push_back(1); // Front Face Tri 1
+    index_list.push_back(1); index_list.push_back(2); index_list.push_back(3); // Front Face Tri 2
 
+    index_list.push_back(2); index_list.push_back(3); index_list.push_back(7); // Bottom Face Tri 1
+    index_list.push_back(7); index_list.push_back(6); index_list.push_back(2); // Bottom Face Tri 2
+
+    // Define some variables for the vao/vbo/shaders
     GLuint vao, vbo_vertices, vbo_indices;
     GLhandleARB shader_program;
 
-    // Create a new vertex array object for the cube
+    // Create a new vertex array object for the cube and bind it to make it active
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
-    // Create a vertex buffer object for this entity
+    // Create a vertex buffer object for this entity, bind it, and populate it with vertex data
     glGenBuffers(1, &vbo_vertices);
     glBindBuffer(GL_ARRAY_BUFFER, vbo_vertices);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)*vert_list.size(), &(vert_list)[0], GL_STATIC_DRAW);
@@ -142,17 +154,19 @@ int main(int argc, char **argv) {
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, x));
 
-    // Create an index buffer object for the cube
+    // Create an index buffer object for the cube, bind it, and populate it with index data
     glGenBuffers(1, &vbo_indices);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo_indices);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Vertex)*index_list.size(), &(index_list)[0], GL_STATIC_DRAW);
 
+    // Create our shader program
     shader_program = glCreateProgramObjectARB();
 
-    // Create the shader objects
+    // Create the vertex/fragment shader
     GLuint vert_shader_id = glCreateShaderObjectARB(GL_VERTEX_SHADER_ARB);
     GLuint frag_shader_id = glCreateShaderObjectARB(GL_FRAGMENT_SHADER_ARB);
 
+    // Load the source for our vert/frag shader
     const char* vert_shader_source = loadShader("shaders/simple_shader.vert");
     const char* frag_shader_source = loadShader("shaders/simple_shader.frag");
 
@@ -164,20 +178,24 @@ int main(int argc, char **argv) {
     glCompileShader(vert_shader_id);
     glCompileShader(frag_shader_id);
 
+    // Bind the attribute at position 0 (see line 154) to the "Vertex" name for the vertex shader
     glBindAttribLocation(shader_program, 0, "Vertex");
+
+    // Bind the first draw buffer to the "FragColor" out variable for the fragment shader
     glBindFragDataLocation(shader_program, GL_DRAW_BUFFER0, "FragColor");
 
     // Attach the vert/frag shader objects to the shader program
     glAttachObjectARB(shader_program, vert_shader_id);
     glAttachObjectARB(shader_program, frag_shader_id);
 
+    // Link the shader program to finalize attachment process
     glLinkProgram(shader_program);
 
     // Check to see if there were any errors in compiling the shaders 
     int infologLength = 0;
     glGetObjectParameterivARB(shader_program, GL_OBJECT_INFO_LOG_LENGTH_ARB, &infologLength);
 
-    // If there were errors, write them to glslinfo.txt
+    // If there were errors, write them to the console
     if (infologLength > 0) {
         int charsWritten = 0;
         char *infoLog;
@@ -210,30 +228,44 @@ int main(int argc, char **argv) {
             }
         }
 
+        // Call some generic window update functions
         updateWindow();
 
+        // Bind the "view_matrix" variable in our C++ program to the "View" variable in the shader
         glUniformMatrix4fv(
                 glGetUniformLocation(shader_program, "View"),
                 1,  
                 false,
                 glm::value_ptr(view_matrix));
 
+        // Bind the "projection_matrix" variable in our C++ program to the "Projection" variable in the shader
         glUniformMatrix4fv(
                 glGetUniformLocation(shader_program, "Projection"),
                 1,  
                 false,
                 glm::value_ptr(projection_matrix));
 
+        // Bind the "model_matrix" variable in our C++ program to the "Model" variable in the shader
         glUniformMatrix4fv(
                 glGetUniformLocation(shader_program, "Model"),
                 1,  
                 false,
                 glm::value_ptr(model_matrix));
 
+        // Make our vertex array active
         glBindVertexArray(vao);
+
+        // Tell the renderer to use our shader program when rendering our object
         glUseProgramObjectARB(shader_program);
+
+        // Render the vao on the screen using "GL_LINE_LOOP"
         glDrawElements(GL_LINE_LOOP, index_list.size(), GL_UNSIGNED_SHORT, (void*)0);
+
+        // All the previous rendering was done on a buffer that's not being displayed on the screen.
+        // SDL_GL_SwapWindow displays that buffer in our window.
         SDL_GL_SwapWindow(main_window);
+
+        // Pause the application for a few milliseconds
         SDL_Delay(10);
     }
 
